72f9f03fede2106aebb77912ff52af5ef68fed39
asad96 <asad1996>
2021-12-22 17:39:52 +0500

    feat: add methods definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,35 @@
 export interface ZendeskSupportPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  initialize(options: InitializeOptions): Promise<void>;
+  setAnonymousIdentity(options: AnonymousOptions): Promise<void>;
+  setIdentity(option: IdentityOption): Promise<void>;
+  showHelpCenter(options: HelpCenterOptions): Promise<void>;
+  showTicketRequest(options: TicketRequestOptions): Promise<void>;
+  showUserTickets(): Promise<void>;
+}
+
+export interface InitializeOptions {
+  zendeskUrl: string,
+  appId: string,
+  clientId: string,
+}
+
+export interface AnonymousOptions {
+  name: string,
+  email: string,
+}
+
+export interface IdentityOption {
+  token: string,
+}
+
+export interface HelpCenterOptions {
+  groupType?: string,
+  groupIds?: number[],
+  labels?: string[]
+}
+
+export interface TicketRequestOptions {
+  subject?: string,
+  tags?: string[],
+  fields?: string[]
 }
--- src/web.ts
+++ src/web.ts
@@ -1,12 +1,26 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { ZendeskSupportPlugin } from './definitions';
+import type { AnonymousOptions, HelpCenterOptions, IdentityOption, InitializeOptions, TicketRequestOptions, ZendeskSupportPlugin } from './definitions';
 
 export class ZendeskSupportWeb
   extends WebPlugin
   implements ZendeskSupportPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+
+  async initialize(options: InitializeOptions): Promise<void> {
+  }
+
+  async setAnonymousIdentity(options: AnonymousOptions): Promise<void> {
+  }
+
+  async setIdentity(option: IdentityOption): Promise<void> {
+  }
+
+  async showHelpCenter(options: HelpCenterOptions): Promise<void> {
+  }
+
+  async showTicketRequest(options: TicketRequestOptions): Promise<void> {
+  }
+
+  async showUserTickets(): Promise<void> {
   }
 }
